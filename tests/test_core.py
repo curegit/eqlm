2bfec3cb510d99b088c7ce6f10f26b83a3d34af0
curegit <37978051+curegit@users.noreply.github.com>
2025-04-05 16:08:52 +0900

    Change test image

--- tests/test_core.py
+++ tests/test_core.py
@@ -9,7 +9,7 @@ class ProcessTest(TestCase):
     def test_biprocess(self):
         for mode, vertical, horizontal, interpolation, target, median, clamp in product(core.Mode, [2, 4], [2, 3], core.Interpolation, [0.0, 0.5, 1.0], [True, False], [True, False]):
             with self.subTest(mode=mode, vertical=vertical, horizontal=horizontal, interpolation=interpolation, target=target, median=median, clamp=clamp):
-                x, _ = eqimg.load_image(filerelpath("../images/macaron.jpg"), normalize=True, orientation=True)
+                x, _ = eqimg.load_image(filerelpath("tsurumai.webp"), normalize=True, orientation=True)
                 f, g = color_transforms(mode.value.color, transpose=True)
                 v = f(x)
                 c = mode.value.channel
