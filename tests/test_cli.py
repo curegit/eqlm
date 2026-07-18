e1e0cb1da4063c1aac70994520e7080e1f4890d3
curegit <37978051+curegit@users.noreply.github.com>
2026-07-19 01:39:31 +0900

    Fix CLI help tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,35 +9,35 @@ class CLITest(TestCase):
     subcommands = ["eq", "match", "laps", "desc"]
 
     def test_no_args(self):
-        with self.assertRaises(SystemExit):
-            with redirect_stderr(i := io.StringIO()):
+        with redirect_stderr(i := io.StringIO()):
+            with self.assertRaises(SystemExit):
                 cli.main(argv=[])
-                self.assertTrue(i.getvalue())
+            self.assertTrue(i.getvalue())
 
     def test_help(self):
-        with self.assertRaises(SystemExit):
-            with redirect_stdout(i := io.StringIO()):
+        with redirect_stdout(i := io.StringIO()):
+            with self.assertRaises(SystemExit):
                 cli.main(argv=["-h"])
-                self.assertTrue(i.getvalue())
+            self.assertTrue(i.getvalue())
 
     def test_version(self):
-        with self.assertRaises(SystemExit):
-            with redirect_stdout(i := io.StringIO()):
+        with redirect_stdout(i := io.StringIO()):
+            with self.assertRaises(SystemExit):
                 cli.main(argv=["-v"])
-                self.assertTrue(i.getvalue())
+            self.assertTrue(i.getvalue())
 
     def test_subcommand_no_args(self):
         for subcommand in self.subcommands:
             with self.subTest(subcommand=subcommand):
-                with self.assertRaises(SystemExit):
-                    with redirect_stderr(i := io.StringIO()):
+                with redirect_stderr(i := io.StringIO()):
+                    with self.assertRaises(SystemExit):
                         cli.main(argv=[subcommand])
-                        self.assertTrue(i.getvalue())
+                    self.assertTrue(i.getvalue())
 
     def test_subcommand_help(self):
         for subcommand in self.subcommands:
             with self.subTest(subcommand=subcommand):
-                with self.assertRaises(SystemExit):
-                    with redirect_stdout(i := io.StringIO()):
+                with redirect_stdout(i := io.StringIO()):
+                    with self.assertRaises(SystemExit):
                         cli.main(argv=[subcommand, "-h"])
-                        self.assertTrue(i.getvalue())
+                    self.assertTrue(i.getvalue())
